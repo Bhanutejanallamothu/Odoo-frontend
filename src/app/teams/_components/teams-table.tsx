92734a3b7b63b41f29c9ac204def80b2125db11e
Meghavarshini Prathapani <meghavarshiniprathapani@gmail.com>
2025-12-27 08:46:41 +0000

    it is not reflected on teams page check and add

--- src/app/teams/_components/teams-table.tsx
+++ src/app/teams/_components/teams-table.tsx
@@ -43,7 +43,6 @@ export default function TeamsTable({ teams, users, onEdit, onDelete }: TeamsTabl
             <TableHead>Team Name</TableHead>
             <TableHead>Team Members</TableHead>
             <TableHead>Company</TableHead>
-            <TableHead>Actions</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
@@ -52,27 +51,6 @@ export default function TeamsTable({ teams, users, onEdit, onDelete }: TeamsTabl
               <TableCell className="font-medium">{team.name}</TableCell>
               <TableCell>{getTeamMemberNames(team.memberIds)}</TableCell>
               <TableCell>My Company (San Francisco)</TableCell>
-              <TableCell>
-                <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <Button variant="ghost" className="h-8 w-8 p-0">
-                      <span className="sr-only">Open menu</span>
-                      <MoreHorizontal className="h-4 w-4" />
-                    </Button>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent align="end">
-                    <DropdownMenuItem onClick={() => onEdit(team)}>
-                      <Pencil className="mr-2 h-4 w-4" /> Edit
-                    </DropdownMenuItem>
-                    <DropdownMenuItem
-                      onClick={() => onDelete(team)}
-                      className="text-destructive"
-                    >
-                      <Trash2 className="mr-2 h-4 w-4" /> Delete
-                    </DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-              </TableCell>
             </TableRow>
           ))}
         </TableBody>
